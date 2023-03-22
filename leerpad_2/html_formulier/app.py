b7dc39b6417011eeb41a47c0946525090f2169cb
mike spaans <99069233@mydavinci.nl>
2023-03-22 10:03:44 +0100

    HTML formulier met validatie onderdeel 2 afgemaakt

--- leerpad_2/html_formulier/app.py
+++ leerpad_2/html_formulier/app.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
 
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
-            ]
 
 # url_for('static', filename='style.css')
 
@@ -14,7 +9,6 @@ messages = [{'title': 'Message One',
 @app.route('/')
 def index():
     # username = request.args.get('username')
-    print ("test")
     return render_template(template_name_or_list='welcome.html')
 
     # return render_template(template_name_or_list='welcome.html')
@@ -22,4 +16,5 @@ def index():
 def welcome():
     name = request.form.get("username", "")
     email = request.form.get("email", "")
-    return f'<h1>De ingevulde gegevens zijn</h1> <p> Naam : {name} <br> email : {email}</p>'
+    print (type(name))
+    return render_template(template_name_or_list='response.html', name=name, email=email)
